7da39eff38901b0d44ed969c54494ceefa21f8cf
tlarbals824 <simkm0824@naver.com>
2023-03-15 20:36:52 +0900

    feat : add ch02-ex3

---
+++ ch02/ch02-ex3/src/main/java/com/sim/ch02ex3/config/ProjectConfig.java
@@ -0,0 +1,46 @@
+package com.sim.ch02ex3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * spring security 5.7.0 이후로 WebSecurityConfigurerAdapter가 deprecated 되서
+ * @EnableWebSecurity를 이용해서 설정했음
+ * 아래 주소로 들어가면 @EnableWebSecurity로 변경하는 방법을 설명해줌
+ * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+ */
+@Configuration
+@EnableWebSecurity
+public class ProjectConfig{
+
+
+	// 아래 방법을 통한 통합 설정은 보다는 ex2에서 사용한 빈 등록을 통한 설정을 해야한다.
+	/*
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		var userDetailsService = new InMemoryUserDetailsManager();
+
+		var user = User.withUsername("john")
+			.password("12345")
+			.authorities("read")
+			.build();
+
+		userDetailsService.createUser(user);
+
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+	*/
+
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeHttpRequests()
+			.anyRequest().authenticated();
+
+		return http.build();
+	}
+}
