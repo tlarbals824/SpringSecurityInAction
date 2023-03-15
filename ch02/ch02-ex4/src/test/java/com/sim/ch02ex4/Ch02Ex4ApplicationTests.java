e5886e877e7a8a6c2f167147c7e90e57e0f8c7db
tlarbals824 <simkm0824@naver.com>
2023-03-15 20:37:35 +0900

    feat : add ch02-ex4

---
+++ ch02/ch02-ex4/src/test/java/com/sim/ch02ex4/Ch02Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.ch02ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02Ex4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
