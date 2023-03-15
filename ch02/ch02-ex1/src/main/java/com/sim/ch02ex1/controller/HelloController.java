ac47d3cb6133e0012d751b1a04c67dc877700985
tlarbals824 <simkm0824@naver.com>
2023-03-15 20:05:51 +0900

    style : move ch02-ex1 to ch02

--- ch02/ch02-ex1/src/main/java/com/sim/ch02ex1/controller/HelloController.java
+++ ch02/ch02-ex1/src/main/java/com/sim/ch02ex1/controller/HelloController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	// curl -u user:passowrd http://localhost:8080/hello
+	// password에 경우 spring 시작시에 생성해주는 UUID를 사용한다.
 	@GetMapping("/hello")
 	public String hello(){
 		return "Hello";
