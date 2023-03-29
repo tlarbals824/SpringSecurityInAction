7f9ac04c3a110800ecbdc92beeed259c9027c995
tlarbals824 <simkm0824@naver.com>
2023-03-29 16:07:30 +0900

    feat : add ch08-ex3 with Gradle

---
+++ ch08/ch08ex3/src/main/java/com/sim/ch08ex3/Ch08ex3Application.java
@@ -0,0 +1,13 @@
+package com.sim.ch08ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch08ex3Application.class, args);
+	}
+
+}
---
+++ ch08/ch08ex3/src/main/java/com/sim/ch08ex3/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.sim.ch08ex3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService(){
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+		UserDetails adminUser = User.withUsername("admin")
+			.password("test")
+			.roles("ADMIN")
+			.build();
+
+		UserDetails managerUser = User.withUsername("manager")
+			.password("test")
+			.roles("MANAGER")
+			.build();
+
+		manager.createUser(adminUser);
+		manager.createUser(managerUser);
+
+		return manager;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.httpBasic();
+
+		// HTTP POST 방식으로 /a 경로를 호출할 수 있게 CSRF 비활성화
+		http.csrf().disable();
+
+		// 선택기로 요청을 참조할 때 특정한 규칙부터 일반적인 규칙의 순서로 지정해야 한다.
+		http.authorizeRequests(
+			request -> request
+				// /a/b/** 은 /a/b가 붙은 모든 경로를 나타낸다.
+				.mvcMatchers("/a/b/**").authenticated()
+				.anyRequest().permitAll()
+		);
+
+		return http.build();
+	}
+}
