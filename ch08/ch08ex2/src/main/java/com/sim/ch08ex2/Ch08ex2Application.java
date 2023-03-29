0cfb4515458fc5d1c39ad6b5afd519b4705cede7
tlarbals824 <simkm0824@naver.com>
2023-03-29 16:07:01 +0900

    feat : add ch08-ex2 with Gradle

---
+++ ch08/ch08ex2/src/main/java/com/sim/ch08ex2/Ch08ex2Application.java
@@ -0,0 +1,13 @@
+package com.sim.ch08ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch08ex2Application.class, args);
+	}
+
+}
---
+++ ch08/ch08ex2/src/main/java/com/sim/ch08ex2/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.sim.ch08ex2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@PostMapping("/a")
+	public String postEndpointA(){
+		return "Works!";
+	}
+
+	@GetMapping("/a")
+	public String getEndpointA(){
+		return "Works!";
+	}
+
+	@GetMapping("/a/b")
+	public String getEndpointB(){
+		return "Works!";
+	}
+
+	@GetMapping("/a/b/c")
+	public String getEndpointC(){
+		return "Works!";
+	}
+
+
+}
